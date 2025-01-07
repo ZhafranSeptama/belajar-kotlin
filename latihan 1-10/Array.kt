a317416c45f9386ef50f502d0c0fa071a9af032f
japran <zhafranseptama04@gmail.com>
2025-01-07 18:15:52 +0700

    Add files via upload

---
+++ latihan 1-10/Array.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    val names: Array<String> = arrayOf("Eko", "Joko", "Budi")
+    // names.set(0, "Rudi")
+    println(names[0])
+    names[0] = "Rudi"
+    println(names[0])
+    println(names[1])
+
+
+    val nilai: Array<Int> = arrayOf(10,9,8)
+    println(nilai[0])
+    println(nilai[1])
+    println(nilai[2])
+
+    val members: Array<String?> = arrayOfNulls(5)
+    members[0] = "Eko"
+    members[1] = "Eko"
+    members[2] = "Eko"
+    members[3] = null
+    members[4] = "Eko"
+    println(members.size)
+
+}
